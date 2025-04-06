ac3c93564c0ebf625288b9cf7f5852fb7b115dda
Dean Lau <ahdeanlau@gmail.com>
2025-04-06 17:07:25 +0800

    remove: unused line

--- embeddings/pdfchunker.py
+++ embeddings/pdfchunker.py
@@ -43,10 +43,10 @@ class PDFChunker:
             chunks = text_splitter.split_text(combined_document)
             self.logger.info(f"Split the document into {len(chunks)} chunks.")
             self.logger.debug(f"First chunk preview: {chunks[0][:300]}")
-
+")
             # Write chunks to file
             return chunks
-            self.logger.info(f"Successfully return all chunks.")
+            
 
         except Exception as e:
             self.logger.error("An error occurred during processing.", exc_info=True)
