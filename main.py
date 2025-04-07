f2a6ee7e7b51f792563d220df0e2c94d5319e37b
Dean Lau <ahdeanlau@gmail.com>
2025-04-07 09:57:06 +0800

    feat: create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from openai import OpenAI
+from config.config_env import OPENAI_API_KEY
+
+openai_client = OpenAI(api_key=OPENAI_API_KEY)
+
+response = openai_client.embeddings.create(
+    input="Hello",
+    model="text-embedding-3-small"
+)
+
+print(response.data[0].embedding)
